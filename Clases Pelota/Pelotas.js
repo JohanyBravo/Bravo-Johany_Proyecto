f3f7c4cfa4da2f772c5147f23a937d69834a92e6
Johany Bravo <mayli.bravo@ucuenca.edu.ec>
2023-12-22 00:30:45 -0500

    Se agregó  cuadrados y color de fondo

--- Clases Pelota/Pelotas.js
+++ Clases Pelota/Pelotas.js
@@ -1,9 +1,11 @@
 class Pelotas {
   constructor() {
     this.lifespan = Math.ceil(random(10, 100));
-    this.c = color(random(150), random(10, 255), 10);
+    this.c = color(random(205), random(200, 250), 200);
     this.margen = 100;
     this.step = 0.001;
+    this.isDead = false;
+    this.espera = 30;
 
     this.pos1 = createVector(
       random(this.margen, width - this.margen),
@@ -48,9 +50,25 @@ class Pelotas {
     this.distancia = this.pos1.dist(this.pos2);
     if (this.distancia < this.colision) {
       circle(this.pos3.x, this.pos3.y, this.diam3);
+      this.espera -= 1;
+      if (this.espera <= 0) {
+        this.creciendo = true;
+      }
+      this.isDead = true;
     } else {
-      circle(this.pos1.x, this.pos1.y, this.diam1);
+      // square(this.pos1.x, this.pos1.y, this.diam1);
+      // ellipse(this.pos2.x, this.pos2.y, this.diam3, 5);
+      // fill(255);
       circle(this.pos2.x, this.pos2.y, this.diam2);
+      fill(255, 250, 0);
+      circle(this.pos1.x, this.pos1.y, this.diam1, 10);
+      const c = color(255, 204, 0);
+      fill(c);
+      // rect(15, 20, 35, 60);
+      // Sets 'redValue' to 255.
+      const redValue = red(c);
+      fill(redValue, 0, 0);
+      square(this.pos3.x, this.pos3.y, this.diam1, 1);
     }
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,24 +1,36 @@
-Pelotas = [];
-let p1;
+let pelotas = [];
 // let p2;
 let x = 10;
 let y = x;
-// let posX;
-// let posY;
-// let vel = 10;
-// let radio = diametro / 2;
+
+let temp = 3;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
-
-  p1 = new Pelotas();
+  let np = new Pelotas();
+  pelotas.push(np);
+  backgroundColor = color(0, 0, 0);
 }
 
 function draw() {
-  background(0);
-  p1.update();
-  p1.display();
-  // p2.update();
-  // p2.display();
+  // background();
+  if (frameCount % temp == 0) {
+    let np = new Pelotas();
+    print("hola");
+    pelotas.push(np);
+    backgroundColor = lerpColor(
+      backgroundColor,
+      color(random(255), random(1), random(250)),
+      0.3
+    );
+    background(backgroundColor);
+  }
+  for (let i = 0; i < pelotas.length; i++) {
+    pelotas[i].update();
+    pelotas[i].display();
+    if (pelotas[i].isDead) {
+      pelotas.splice(i, 1);
+    }
+  }
 }
